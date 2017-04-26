ae93d234fecd45cf6cf17d3ac4b13edf6ac0ecf6
Victor Tran <vicr12345@gmail.com>
2017-04-26 14:42:34 +0000

    Initial welcome screen

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "welcomewindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    WelcomeWindow w;
+    w.show();
+
+    return a.exec();
+}
